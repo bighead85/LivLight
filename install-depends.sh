55e025f18b0dfb4c6dff59dadae24dacea52838c
bighead85 <big_head85@hotmail.com>
2016-05-12 12:43:22 +0000

    update depends

--- install-depends.sh
+++ install-depends.sh
@@ -11,4 +11,6 @@ sudo python setup.py install
 cd ../
 sudo apt-get install mplayer --yes
 sudo gpio load i2c
-sudo ./piglow.py
+sudo ./piglow.py 
+sudo echo "i2c-dev" >> /etc/modules 
+sudo echo "i2c-bcm2708" >> /etc/modules 
